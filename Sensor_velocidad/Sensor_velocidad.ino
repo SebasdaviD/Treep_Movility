0072de39f6c556136f01281cd11716baa343557f
unknown <sdmazoc@unal.edu.co>
2021-06-16 08:41:16 -0500

    Se agrega código de sensor de velocidad en dos ńucleos

---
+++ Sensor_velocidad/Sensor_velocidad.ino
@@ -0,0 +1,85 @@
+//#include <Arduino.h>
+
+TaskHandle_t Task1;//"Task1" nombre de la tarea del proceso
+
+ int HALLPin = 17;
+ int LEDPin = 16;
+ float rpm=0;
+ 
+ int medida=0;
+ int count=0;
+ int countx;
+ int deltat=0;
+ unsigned long tiempo=0;
+ unsigned long NOW; 
+ boolean banderavel=false; 
+
+void setup() {
+ pinMode(HALLPin, INPUT_PULLUP);
+  //Crear el nuevo hilo del proceso
+  xTaskCreatePinnedToCore(
+    loop2,    //Nombre del loop creado "loop2"
+    "Task_1", //Nombre
+    10000,     //Tamaño de la pila, aumentar tamaño si sale error
+    NULL,     //Parametro casi siempre Nulo
+    1,        //Prioridad de la tarea
+    &Task1,   //Nombre de la tarea "Task1"
+    0);       //Nucleo donde se ejecuta
+  Serial.begin(115200);
+
+}
+
+// hilo 0 
+void loop() {
+  delay(1000);
+}
+
+// hilo 1
+void loop2(void *parameter)
+{
+ for (;;)
+   {
+    Velocidad();
+    banderavel=false;
+   } // del for infinito
+}
+
+
+void Velocidad(){
+  while(banderavel == false )
+    {
+      delay(5); // Retraso para evitar medir ruido
+      
+      if(digitalRead(HALLPin)==1)   // Significa que el sensor está en estado BAJO
+        {
+        countx=0;   
+        }
+        
+        // HALLPin=0 Significa que el sensor es afectado por el imán (Estado alto)
+        if(digitalRead(HALLPin)==0 && countx==0)
+        {
+        Serial.println("Alto");
+        countx=1;       // Variable auxiliar usada más adelante (significa que entramos a este estado)
+        count++;        // sumamos en uno el contador (en este caso sirve por si ponemos más de un imán)
+
+          if(count==1)    // Imán pasa por primera vez  
+          {   
+          NOW=millis();   // Obtenemos el tiempo actual para la tarjeta 
+          }
+          
+          // El contador puede aumentar dependiendo de la cantidad 
+          // de imanes que tengamos para una mejor medida
+          if(count==2)
+          {
+          tiempo=millis(); // Tomamos el tiempo la segunda vez que pasó el imán por el sensor
+          deltat=tiempo-NOW; // Calculamos el tiempo entre dos altos del sensor
+          rpm=(count-1)*(60000/(float)deltat);  // Calculamos las rpm del motor
+          count=0;           // Reiniciamos la variable contador para entrar en el primer if
+          Serial.println(rpm);    // Poner la información de los rpm
+          Serial.println(deltat);
+          banderavel=true;
+          }
+        }
+      }
+      vTaskDelay(10);
+}
