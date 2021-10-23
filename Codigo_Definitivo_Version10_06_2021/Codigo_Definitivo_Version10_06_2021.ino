13a11c6fa3ae198029abccf91288aaa0d7203831
unknown <sdmazoc@unal.edu.co>
2021-10-22 19:52:41 -0500

    Agregado al codigo principal el modulo de lectura SD con sus respectivas librerias

--- Codigo_Definitivo_Version10_06_2021/Codigo_Definitivo_Version10_06_2021.ino
+++ Codigo_Definitivo_Version10_06_2021/Codigo_Definitivo_Version10_06_2021.ino
@@ -2,6 +2,9 @@
  * Elaborado por Sebastian David Mazo Cadavid con Ayuda de Fabio Vahos de Tecnoparque del Sena
  * Treep Mobility 2021 última revisión en 9 de Junio de 2021
  * Placa de desarrollo: Widora Air Versión 6.0  
+ * 
+ * Se debe seleccionar la placa ---> ESP32 Arduino ---> ESP32 Dev Module
+ * 
  */
 
 // Librerías %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -10,12 +13,16 @@
 #include <Wire.h>             // Librería para hacer comunicaciones I2C con distintos dispositivos
 #include <MPU9255.h>          // Librería para el uso de magnetómetro, giroscopio y magnetómetro
 #include "Adafruit_VL53L0X.h" // Libreria para hacer uso del sensor laser para medir distancias
+#include "FS.h"               // Librería para hacer uso de módulo SD
+#include "SD.h"               // Librería para hacer uso de módulo SD
+#include "SPI.h"              // Librería para usar comunicaciones SPI y aplicar en módulo SD
 
 // Definición de pines de Entrada-Salida %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 
 #define I2C_SDA 21    // Pin de comunicación I2C de datos
 #define I2C_SCL 22    // Pin de comunicación I2C de Reloj
+#define csPin 5       // Pin seleccionador de módulo SD para comunicaciones SPI
 
 
 // Definición de variables globales %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -35,6 +42,7 @@ void setup() {
   Wire.begin(I2C_SDA, I2C_SCL);   // Definición de los pines I2C de la placa de desarrollo
   if (!lox.begin()) {
   Serial.println(F("Error al iniciar VL53L0X"));
+  Sd_init();
   }
 }
 
@@ -43,13 +51,8 @@ void setup() {
 void loop() {
 //Canal(2);           // llamamos al primer sensor ubicado en la parte x de la moto
 //Acelerometro();     // Lectura de las aceleraciones
-//delay(100);        // Esperamos 1 segundo para la siguiente medida     
-// Canal(1);
-// Serial.println("Canal1");
-// Acelerometro();
-// delay(100);        // Esperamos 1 segundo para la siguiente acción
-Laser();
-delay(200);
+datalog();
+delay(90000);
 }
 
 
@@ -142,6 +145,222 @@ void Laser(){
     } 
   }
 // %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+
+
+// %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%Escritura SD%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+void datalog(){
+  writeFile(SD, "/datta.txt", "Hello ");      // Escribe en la´primera linea del archivo hello.txt
+  appendFile(SD, "/datta.txt", "World!\n");   // Escribe en la siguiente linea de hello.txt (se debe
+                                              // repetir para cada dato que se ponga en la SD)
+                                              
+}
+        
+
+// %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+
+
+
+// %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%Inicializacion modulo SD%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+void Sd_init(){
+  if(!SD.begin(csPin)){
+    Serial.println("Card Mount Failed");
+    return;
+  }
+  uint8_t cardType = SD.cardType();
+
+  if(cardType == CARD_NONE){
+    Serial.println("No SD card attached");
+    return;
+  }
+
+  Serial.print("SD Card Type: ");
+  if(cardType == CARD_MMC){
+    Serial.println("MMC");
+  } else if(cardType == CARD_SD){
+    Serial.println("SDSC");
+  } else if(cardType == CARD_SDHC){
+    Serial.println("SDHC");
+  } else {
+    Serial.println("UNKNOWN");
+  }
+
+  uint64_t cardSize = SD.cardSize() / (1024 * 1024);
+  Serial.printf("SD Card Size: %lluMB\n", cardSize);
+
+}
+
+// %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+
+
+
+// %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% Funciones internas de modulo SD %%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+void listDir(fs::FS &fs, const char * dirname, uint8_t levels){
+  Serial.printf("Listing directory: %s\n", dirname);
+
+  File root = fs.open(dirname);
+  if(!root){
+    Serial.println("Failed to open directory");
+    return;
+  }
+  if(!root.isDirectory()){
+    Serial.println("Not a directory");
+    return;
+  
+  }
+
+  File file = root.openNextFile();
+  while(file){
+    if(file.isDirectory()){
+      Serial.print("  DIR : ");
+      Serial.println(file.name());
+      if(levels){
+        listDir(fs, file.name(), levels -1);
+      }
+    } else {
+      Serial.print("  FILE: ");
+      Serial.print(file.name());
+      Serial.print("  SIZE: ");
+      Serial.println(file.size());
+    }
+    file = root.openNextFile();
+  }
+}
+
+void createDir(fs::FS &fs, const char * path){
+  Serial.printf("Creating Dir: %s\n", path);
+  if(fs.mkdir(path)){
+    Serial.println("Dir created");
+  } else {
+    Serial.println("mkdir failed");
+  }
+}
+
+void removeDir(fs::FS &fs, const char * path){
+  Serial.printf("Removing Dir: %s\n", path);
+  if(fs.rmdir(path)){
+    Serial.println("Dir removed");
+  } else {
+    Serial.println("rmdir failed");
+  }
+}
+
+void readFile(fs::FS &fs, const char * path){
+  Serial.printf("Reading file: %s\n", path);
+
+  File file = fs.open(path);
+  if(!file){
+    Serial.println("Failed to open file for reading");
+    return;
+  }
+
+  Serial.print("Read from file: ");
+  while(file.available()){
+    Serial.write(file.read());
+  }
+  file.close();
+}
+
+void writeFile(fs::FS &fs, const char * path, const char * message){
+  Serial.printf("Writing file: %s\n", path);
+
+  File file = fs.open(path, FILE_WRITE);
+  if(!file){
+    Serial.println("Failed to open file for writing");
+    return;
+  }
+  if(file.print(message)){
+    Serial.println("File written");
+  } else {
+    Serial.println("Write failed");
+  }
+  file.close();
+}
+
+void appendFile(fs::FS &fs, const char * path, const char * message){
+  Serial.printf("Appending to file: %s\n", path);
+
+  File file = fs.open(path, FILE_APPEND);
+  if(!file){
+    Serial.println("Failed to open file for appending");
+    return;
+  }
+  if(file.print(message)){
+      Serial.println("Message appended");
+  } else {
+    Serial.println("Append failed");
+  }
+  file.close();
+}
+
+void renameFile(fs::FS &fs, const char * path1, const char * path2){
+  Serial.printf("Renaming file %s to %s\n", path1, path2);
+  if (fs.rename(path1, path2)) {
+    Serial.println("File renamed");
+  } else {
+    Serial.println("Rename failed");
+  }
+}
+
+void deleteFile(fs::FS &fs, const char * path){
+  Serial.printf("Deleting file: %s\n", path);
+  if(fs.remove(path)){
+    Serial.println("File deleted");
+  } else {
+    Serial.println("Delete failed");
+  }
+}
+
+void testFileIO(fs::FS &fs, const char * path){
+  File file = fs.open(path);
+  static uint8_t buf[512];
+  size_t len = 0;
+  uint32_t start = millis();
+  uint32_t end = start;
+  if(file){
+    len = file.size();
+    size_t flen = len;
+    start = millis();
+    while(len){
+      size_t toRead = len;
+      if(toRead > 512){
+        toRead = 512;
+      }
+      file.read(buf, toRead);
+      len -= toRead;
+    }
+    end = millis() - start;
+    Serial.printf("%u bytes read for %u ms\n", flen, end);
+    file.close();
+  } else {
+    Serial.println("Failed to open file for reading");
+  }
+
+
+  file = fs.open(path, FILE_WRITE);
+  if(!file){
+    Serial.println("Failed to open file for writing");
+    return;
+  }
+
+  size_t i;
+  start = millis();
+  for(i=0; i<2048; i++){
+    file.write(buf, 512);
+  }
+  end = millis() - start;
+  Serial.printf("%u bytes written for %u ms\n", 2048 * 512, end);
+  file.close();
+}
+
+
+// %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
 
 
 // Función para medir la distancia mediante el sensor laser 
